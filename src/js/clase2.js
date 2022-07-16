105705008491c61be8df84600a71b715c1adf2e9
Jmraggi <91738633+Jmraggi@users.noreply.github.com>
2022-07-16 18:49:32 -0300

    Clase1

---
+++ src/js/clase2.js
@@ -0,0 +1,98 @@
+///* ========================================================================================== */
+///* ================================Control de flujos========================================= */
+///* ========================================================================================== */
+
+
+
+///* ==================================EJERCITACIONES========================================== */
+
+
+//Se declaran las variables, sean locales o no
+// var nombre = "Juan Manuel";
+// let apellido = "Raggi"; 
+// const DNI = 39098714;           //las constantes ponerlas en MAYUSCULA para poder diferenciarlas
+// let direccion;                  
+// direccion = "Catamarca 1788";
+
+// nombre = "Esteban"; //se pueden cambiar valores de let y var pero no los de const
+// apellido = "Perez";
+
+
+///* =====================================DESAFIO============================================== */
+
+
+//      >> Consigna:Crea un script en JS que le solicite al usuario ingresar mínimo1(un)dato.
+//      Luego,con JavaScript,realiza operaciones matemáticasode concatenación sobre las
+//      entradas teniendo en cuenta el tipo de dato.Al finalizar mostrar el resultados con alert() o console.log()
+
+
+
+
+
+
+
+
+// /*   Declarar una variable significa CREARLA. Para esto usamos la palabras reservadas (var,let o const).
+//      En proramacion no se puede usar acentos, o letra ñ. Dentro de estas variables podemos guardar valores,
+//      Se puede asociar con number (valor numerico), string (valor de texto).
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Let y Const<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+//      Let y const son declaraciones, una vriable let puede cambiar en el  trasncurso de la aplicacion,
+//      Una constante (Const) recibe una unica asigancion. la diferencia entre let,const y var, es que
+//      esta ultima tiene un scope global.
+
+
+//  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Operaciones basicas<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// //     Con variables de valores numericos, podes realizar operaciones matematicas: sumas, restas, etc.
+
+// let textoA = "CODER";
+// let textoB = "HOUSE";
+// const BLANCO = 11;
+// // Concatenar textoAytextob("CODER"+"HOUSE"="CODERHOUSE")
+// let resultadoA=textoA+textoB;
+// // Concatenar textoBy1("HOUSE"+1 = "HOUSE1)"')
+// let resultadoB=textoB+ 1;
+// // Concatenar textoA,BLANCOytextoB("CODER" + "" + "HOUSE"="CODER HOUSE")
+// let resultadoC=textoA+BLANCO+textoB;
+
+// console.log(resultadoA);
+// console.log(resultadoB);
+// console.log(resultadoC);
+                                      
+
+//* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Operaciones basicas<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// //     Con variables de valores numericos, podes realizar operaciones matematicas: sumas, restas, etc.
+
+// let textoA = "CODER";
+// let textoB = "HOUSE";
+// const BLANCO = 11;
+// // Concatenar textoAytextob("CODER"+"HOUSE"="CODERHOUSE")
+// let resultadoA=textoA+textoB;
+// // Concatenar textoBy1("HOUSE"+1 = "HOUSE1)"')
+// let resultadoB=textoB+ 1;
+// // Concatenar textoA,BLANCOytextoB("CODER" + "" + "HOUSE"="CODER HOUSE")
+// let resultadoC=textoA+BLANCO+textoB;
+
+// console.log(resultadoA);
+// console.log(resultadoB);
+// console.log(resultadoC);
+
+
+//* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Prompt,consola,alert<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// /*   prompt() mostrar un cuadro de dialogo para que el usuario ingrese un dato.
+//      ejemplo: let nombreIngresado = prompt("Ingrese su nombre porfavor");
+//      Esto queda almacenado en la variable nombreIngresado
+
+//      console.log() muestra el mensaje en la consola
+//      ejemplos: console.log("Mensaje de prueba") >>Yo le digo que mostrar
+//      let nombre = "juan perez" 
+//      console.log(nombre) >> Le paso por parametros lo que quiero que muestre
+
+//      alert() muestra el mensaje en una ventana emergente
+//      ejemplos: alert("Mensaje de prueba") >>Yo le digo que mostrar
+
+
